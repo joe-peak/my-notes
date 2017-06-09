514a5e3196e08b715f98f0de055b83e7c0f3114b
周长龙 <zhouchanglong@meeidol.com>
2017-06-09 13:45:50 +0800

    node notes

--- node/server.js
+++ node/server.js
@@ -1,13 +1,28 @@
-const http=require('http');
-const port='3000';
-const hostname='localhost';
+const http = require('http');
+const port = '3000';
+const hostname = 'localhost';
 
-let server=http.createServer((req,res)=>{
-  res.writeHead('200',{'Content-Type':'text/html'});
+/*let server = http.createServer((req, res) => {
+  res.writeHead('200', { 'Content-Type': 'text/html' });
   res.write('<h1>Node Js</h1>');
   res.end('<p>Tourial starts here.</p>');
 });
 
-server.listen(port,hostname,()=>{
+server.listen(port, hostname, () => {
+  console.log(`server is running on http://${hostname}:${port}`);
+});*/
+
+/**
+ * 创建http服务器实例
+ */
+let serverCopy = new http.Server();
+/**
+ * 监听request事件
+ */
+serverCopy.on('request', (req, res) => {
+  res.writeHead('200', { 'Content-Type': 'text/html' });
+  res.write('<h1>Node Js Copy</h1>');
+  res.end('<p>Hello Node</p>');
+}).listen(port, hostname, () => {
   console.log(`server is running on http://${hostname}:${port}`);
 });
